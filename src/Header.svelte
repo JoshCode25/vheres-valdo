06a9f6a44ab4ec5a84fc11b41c6646d9e94535d7
JoshG <joshcode25@gmail.com>
2022-11-17 22:02:22 -0600

    modified timerbar to adjust width and color based on remaining time

--- src/Header.svelte
+++ src/Header.svelte
@@ -6,30 +6,73 @@
   export let highScore = 5;
 
   let timerId;
+  let timerActive = false;
+  let timerBarColorList = ['green', 'orange', 'red'];
+  let timerBarColor = timerBarColorList[0];
 
-  function startTimer() {
-    timerId = setInterval(checkTimer, 1000);
+  $: timePercent = Math.floor((remainingTime / maxTime) * 1000) / 10;
+  $: if (timePercent < 50 && timerBarColor === timerBarColorList[0]) {
+    timerBarColor = timerBarColorList[1];
+    console.log(timePercent, timerBarColor);
+  } else if (timePercent < 10 && timerBarColor === timerBarColorList[1]) {
+    timerBarColor = timerBarColorList[2];
+    console.log(timePercent, timerBarColor);
   }
-  function clearTimer() {
-    clearInterval(timerId);
+  $: timerBarStyle = `--width: ${timePercent}%; --background-color: ${timerBarColor}`;
+
+  function roundToTenth(number) {
+    let multByTen = number * 10;
+    let roundedTen = Math.floor(multByTen);
+    let roundedToTenth = roundedTen / 10;
+    return roundedToTenth;
   }
-  function checkTimer() {
-    if (remainingTime > 0) {
-      remainingTime = remainingTime - 1;
-      console.log(remainingTime);
-    } else if (remainingTime === 0) {
-      clearTimer();
+  function toggleTimer() {
+    if (timerActive) {
+      stopTimer(timerId);
+    } else {
+      timerId = setInterval(() => checkTimer(timerId), 100);
     }
+  }
+  function checkTimer(id) {
+    timerActive = true;
+
+    if (remainingTime > 0) {
+      remainingTime = roundToTenth(remainingTime - 0.1);
+    } else if (remainingTime === 0) {
+      stopTimer(id);
+    }
+  }
+
+  function stopTimer(id) {
+    clearInterval(id);
+    timerActive = false;
   }
 </script>
 
 <style>
   header {
+    position: relative;
     display: flex;
     justify-content: space-between;
+    align-items: center;
   }
 
-  h3 {
+  header > * {
+    margin: 5px;
+  }
+  .timerBarWrapper {
+    --width: 100%;
+    --background-color: green;
+  }
+  .timerBar {
+    margin: 5px 0px;
+    left: 0;
+    height: 20px;
+    width: var(--width, 100%);
+    background-color: var(--background-color, green);
+  }
+  h3,
+  button {
     flex-grow: 1;
   }
 
@@ -41,7 +84,10 @@
 <header>
   <h3>Current Score: {score}</h3>
   <h4>Current Valdo: {valdoName}</h4>
-  <h3 on:click={startTimer}>Time Setting: {maxTime}s</h3>
+  <button on:click={toggleTimer}>Time Setting: {maxTime}s</button>
+  <h3>Remaining Time: {remainingTime} {timePercent}%</h3>
   <h3>High Score: {highScore}</h3>
 </header>
-<div>Remaining Time: {remainingTime}</div>
+<div class="timerBarWrapper" style={timerBarStyle}>
+  <div class="timerBar" />
+</div>
