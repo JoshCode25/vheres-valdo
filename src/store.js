432af9cbf514927ebb680c360adf592084e7d6a8
JoshG <joshcode25@gmail.com>
2022-11-18 07:08:04 -0600

    initial erroneous attempt to add method to gameTime store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { writable } from "svelte/store";
 
+const defaultMaxTime = 90;
+
 export const gameTime = writable({
-    maxTime: 90, 
-    remainingTime: maxTime
+    maxTime: defaultMaxTime, 
+    remainingTime: defaultMaxTime,
+    percentRemaining: Math.floor((remainingTime / maxTime) * 1000) / 10;
 });
