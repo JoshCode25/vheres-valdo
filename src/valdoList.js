8c88073e417726c5b57f7bb99e07502229dd5a92
JoshG <joshcode25@gmail.com>
2022-11-19 16:44:09 -0600

    Started list of Valdos

---
+++ src/valdoList.js
@@ -0,0 +1,4 @@
+export default valdoList = [
+    {firstName: 'Jenathon', lastName: 'Hatmoore', greeting: 'Wa-heya!', correct:'Dasa\' me!', incorrect:'Nosa\' me!' },
+    {firstName: 'Brinkle', lastName: 'Sinkle', greeting: 'Ye bop', correct:'whoya', incorrect:'boo-yo' },
+]
