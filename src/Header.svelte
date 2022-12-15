4821cbc7791a3a32f3ee52e21cfb8fb34f3de2f2
JoshG <joshcode25@gmail.com>
2022-12-15 12:57:30 -0600

    rearranged and consolidated header layout

--- src/Header.svelte
+++ src/Header.svelte
@@ -38,31 +38,40 @@
     valdoStore.finishGame();
     console.log('finished game:', completedGame, $valdoStore, $gameTimer);
   }
-  $: activeValdoData = $valdoStore.activeValdo;
   $: console.log($valdoStore.activeValdo);
 </script>
 
 <header>
-  <h3>Current Score: {$gameTimer.score}</h3>
+  <div class="gameInfoWrapper">
+    <h3>Current Score: {$gameTimer.score}</h3>
+    <h3>High Score: {$gameTimer.highScore}</h3>
+    <h3>
+      Remaining Time: {$gameTimer.remainingTime}s
+    </h3>
+  </div>
   <div>
-    <h4>Current Valdo:</h4>
-    {#if $valdoStore.activatedGame}
-      <Valdo height={100} valdoData={activeValdoData} {displaySVG} />
-    {:else}
-      <h4>Start to See!</h4>
+    {#if !$valdoStore.activatedGame && !completedGame}
+      <button class="hoverPointer" on:click={startTimer}>Click to Start</button>
+    {:else if $valdoStore.activatedGame}
+      <h2>Current Valdo:</h2>
+    {:else if completedGame}
+      <button class="hoverPointer" on:click={startTimer}>Play Again?</button>
     {/if}
   </div>
-  {#if !$valdoStore.activatedGame && !completedGame}
-    <button class="hoverPointer" on:click={startTimer}>Click to Start</button>
-  {:else if $valdoStore.activatedGame}
-    <button>Good Luck!</button>
-  {:else if completedGame}
-    <button class="hoverPointer" on:click={startTimer}>Play Again?</button>
-  {/if}
-  <h3>
-    Remaining Time: {$gameTimer.remainingTime}s
-  </h3>
-  <h3>High Score: {$gameTimer.highScore}</h3>
+  <div class="currentValdoWrapper">
+    {#if $valdoStore.activatedGame}
+      {#key $valdoStore.activeValdo}
+        <Valdo
+          height={100}
+          valdoData={$valdoStore.activeValdo}
+          {displaySVG}
+          hoverPointer={false}
+        />
+      {/key}
+    {:else}
+      <h4>New Valdo Pending...</h4>
+    {/if}
+  </div>
 </header>
 <TimerBar />
 
@@ -70,24 +79,30 @@
   header {
     position: relative;
     display: flex;
-    justify-content: space-between;
+    justify-content: left;
     align-items: center;
+    height: 120px;
+    margin: 0 5px;
+  }
+  header > div {
+    margin-right: 20px;
+  }
+  h2 {
+    font-size: 20px;
   }
 
-  header > * {
+  h3 {
     margin: 5px;
   }
-
-  h3,
-  button {
-    flex-grow: 1;
+  .gameInfoWrapper {
+    text-align: left;
+  }
+  .currentValdoWrapper {
+    display: flex;
+    flex-flow: row-reverse;
   }
 
   button.hoverPointer:hover {
     cursor: pointer;
-  }
-
-  h4 {
-    flex-grow: 3;
   }
 </style>
